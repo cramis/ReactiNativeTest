62b7c34e2bd0d56b46e55bc59d49d3e6cb2856c9
DESKTOP-MYHOME\cramis <cramis@gmail.com>
2018-06-22 00:37:35 +0900

    헤더 만들기

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
+
+const App = () => {
+    return (
+        <Header headerText='Albums!' />
+    );
+};
 
 AppRegistry.registerComponent('albums', () => App);
