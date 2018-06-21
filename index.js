13185273869e6f5f7b11a4ab8e4d0da2d6f87efb
DESKTOP-MYHOME\cramis <cramis@gmail.com>
2018-06-22 02:21:10 +0900

    AlbumList 기초 구현

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
 
 const App = () => {
     return (
-        <Header headerText='Albums!' />
+        <View>
+            <Header headerText='Albums!' />
+            <AlbumList />
+        </View>
     );
 };
 
